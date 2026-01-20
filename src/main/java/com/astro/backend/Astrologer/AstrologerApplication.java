e037c60821b500a56eedc8360617b9012edfbf0c
HappyTyagi <happytyagi3@gmail.com>
2026-01-20 13:02:38 +0530

    first commit

---
+++ src/main/java/com/astro/backend/Astrologer/AstrologerApplication.java
@@ -0,0 +1,13 @@
+package com.astro.backend.Astrologer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrologerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrologerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astro/backend/Astrologer/AstrologerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.backend.Astrologer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrologerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
